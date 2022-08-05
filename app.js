c26f751ecdfb2a77f82b8e37d1b47a595b46eec7
Dhruv Gogna <dhruvgogna01@gmail.com>
2022-08-05 16:53:39 +0530

    final commit

--- app.js
+++ app.js
@@ -22,18 +22,18 @@ const Item = mongoose.model("Item", itemsSchema);
 
 
 const item1 = new Item({
-  name: "Welcome to your todolist!"
+  name: "Welcome to todolist!"
 });
 
-const item2 = new Item({
-  name: "Hit the + button to add a new item."
-});
+// const item2 = new Item({
+//   name: "Hit the + button to add a new item."
+// });
 
-const item3 = new Item({
-  name: "<-- Hit this to delete an item."
-});
+// const item3 = new Item({
+//   name: "<-- Hit this to delete an item."
+// });
 
-const defaultItems = [item1, item2, item3];
+const defaultItems = [item1];
 
 const listSchema = {
   name: String,
